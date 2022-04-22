92eba269c6946ddacc4be2b189927b1592348c5f
Shizlers <w0434675@nscc.ca>
2022-04-22 00:09:58 -0300

    put here

---
+++ form1/form1/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// this just tell the program we are using system input and output(readin and writing files)
+using System.IO;
+
+namespace code_editor_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //
+        // NOTE: the reen text is not needed in your code, im just explaining what everything does with it
+        //
+
+        // 2 forward slash stops the program from reading the current line
+
+        // this clears the richtextbox1
+        private void newToolStripButton_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        // this opens a file and reads to richtextbox1
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Title = "My open file dialog";
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Clear();
+                using (StreamReader sr = new StreamReader(openfile.FileName))
+                {
+                    richTextBox1.Text = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+        }
+
+        // this saves richtextbox1 to a file
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Title = "Save file as..";
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter txtoutput = new StreamWriter(savefile.FileName);
+                txtoutput.Write(richTextBox1.Text);
+                txtoutput.Close();
+            }
+        }
+
+        // this cuts all text highlighted
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        // this copies all text highlighted
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        // this pastes the cut or copied text
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        // this undo's
+        private void undoButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        // this redo's
+        private void redoButton_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Redo();
+        }
+
+        // this highlights all text
+        private void selectAllButton_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.SelectAll();
+        }
+
+        // this calls the new button
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            newToolStripButton.PerformClick();
+        }
+
+        // this calls the open button
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openToolStripButton.PerformClick();
+        }
+
+        // this calls the save button
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveToolStripButton.PerformClick();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveToolStripButton.PerformClick();
+        }
+
+        // this exits the program
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // this calls the undo button
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            undoButton.PerformClick();
+        }
+
+        // this calls the redo button
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            redoButton.PerformClick();
+        }
+
+        // this calls the cut button
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            cutToolStripButton.PerformClick();
+        }
+
+        // this calls the copy button
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            copyToolStripButton.PerformClick();
+        }
+
+        // this calls the paste button
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pasteToolStripButton.PerformClick();
+        }
+
+        // this calls the select all button
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            selectAllButton.PerformClick();
+        }
+
+    }
+}
